1dc61bedae0d1a577a76121a51c45dc2a60dff30
Eladio Acosta <sefirothonu@gmail.com>
2015-03-30 22:11:52 +0200

    cap6-ap3-copia matrices, estrcturas, menuPrin

--- Curso_cpp/curso/estructuras.cpp
+++ Curso_cpp/curso/estructuras.cpp
@@ -7,6 +7,22 @@
 #define EO '~'
 #define FILAS_MAX 10  // número máximo de filas
 #define COLS_MAX  10  // número máximo de columnas
+//-----------variables de copiarmatrices----------------
+#define FILAS 3			// filas
+#define COLS 3			// columnas
+//----------Variables matriz ST ------------------------
+#define NA 100      // número máximo de alumnos
+
+// copia una array en otro
+void static CopiarArrays(float destino[][COLS], float origen[][COLS])
+{
+	int fila = 0, col = 0;
+	for (fila = 0; fila < FILAS; fila++)
+	{
+		for (col = 0; col < COLS; col++)
+			destino[fila][col] = origen[fila][col];
+	}
+}
 
 //matriz unidimensional
 void static matrizUni()
@@ -76,7 +92,8 @@ void static letras()
 void static getCpp()
 {
 	char *c = NULL; // para almacenar el valor retornado por gets
-	char texto[40];
+	char texto[40];
+
 	printf("Introducir líneas de texto.\n");
 	printf("Para finalizar introducir la marca EOF\n\n");
 
@@ -95,29 +112,32 @@ void static matrizBi()
 	float m[FILAS_MAX][COLS_MAX];  // matriz m de dos dimensiones
 	float sumafila;      // suma de los elementos de una fila
 	int filas, cols;     // filas y columnas de la matriz de trabajo
-	int fila, col;       // fila y columna del elemento accedido
+	int fila, col;       // fila y columna del elemento accedido
+
 	do
 	{
-		printf("Número de filas de la matriz:    ");
+		printf("Numero de filas de la matriz:    ");
 		scanf("%d", &filas);
 	}
 	while (filas < 1 || filas > FILAS_MAX);
 	
 	do
 	{
-		printf("Número de columnas de la matriz: ");
+		printf("Numero de columnas de la matriz: ");
 		scanf("%d", &cols);
 	}
 	while (cols < 1 || cols > COLS_MAX);
 
 	// Entrada de datos
-	printf("Introducir los valores de la matriz.\n");
+	printf("Introducir los valores de la matriz.\n");
+
 	for (fila = 0; fila < filas; fila++)
 		for (col = 0; col < cols; col++)
 		{
 			printf("m[%d][%d] = ", fila, col);
 			scanf("%f", &m[fila][col]);
-		}
+		}
+
 	// Escribir la suma de cada fila
 	for (fila = 0; fila < filas; fila++)
 	{
@@ -125,7 +145,8 @@ void static matrizBi()
 		for (col = 0; col < cols; col++)
 			sumafila += m[fila][col];
 		printf("Suma de la fila %d = %g\n", fila, sumafila);
-	}
+	}
+
 	/*
 	for (fila = 0; fila < filas; fila++)
 	{
@@ -143,3 +164,100 @@ void static matrizBi()
 	printf("\nFin del proceso.\n");
 }
 
+// copia el contenido de una matrz en otra
+void static copiarMatrices()
+{
+	static float a[FILAS][COLS], c[FILAS][COLS];
+
+	int fila = 0, col = 0;
+	// Leer datos para la matriz a
+	for (fila = 0; fila < FILAS; fila++)
+	{
+		for (col = 0; col < COLS; col++)
+		{
+			printf("a[%d][%d] = ", fila, col);
+			scanf("%f", &a[fila][col]);
+		}
+	} // llenamos matriz a
+
+	CopiarArrays(a, c);
+
+	// Escribir los datos de la matriz c
+	for (fila = 0; fila < FILAS; fila++)
+	{
+		// Escribir una fila
+		for (col = 0; col < COLS; col++)
+			printf("%10g", c[fila][col]);
+
+		printf("\n"); // fila siguiente
+	}
+}
+
+//--------DEFINICIO DE ESTRUCTURAS-----------------
+typedef struct
+{
+	int dia, mes, anyo;
+} tfecha;
+
+typedef struct
+{
+	char nombre[40];
+	char direccion[50];
+	long telefono;
+	tfecha fecha_nacimiento;
+} tficha;
+// trabajo con estructuras------------------------
+void static estructuras()
+{
+	tficha persona, otraPersona;
+
+	// introduccion de datos de la persona
+	printf("Nombre :  ");
+	scanf("%s", &persona.nombre);
+	printf("Direccion :  ");
+	scanf("%s", &persona.direccion);
+	printf("Telefono :  ");
+	scanf("%ld", &persona.telefono); // conversion a long
+	printf("Fecha de nacimiento \n");
+	printf("Dia :  ");
+	scanf("%d", &persona.fecha_nacimiento.dia);// int
+	printf("Mes :  ");
+	scanf("%d", &persona.fecha_nacimiento.mes);
+	printf("Año :  ");
+	scanf("%d", &persona.fecha_nacimiento.anyo);
+
+	// copia de estucturas
+	otraPersona = persona;
+
+	// escribir los datos de la estructura copiada
+	printf("\n\n---------------\n");
+	printf("Nombre : %s \n", otraPersona.nombre);
+	printf("Direccion : %s \n", otraPersona.direccion);
+	printf("Telefono : %ld \n", otraPersona.telefono);
+	printf("Fecha Nac:  :");
+	printf("%d / ", otraPersona.fecha_nacimiento.dia);
+	printf("%d / ", otraPersona.fecha_nacimiento.mes);
+	printf("%d \n", otraPersona.fecha_nacimiento.anyo);
+}
+//-------DEFINICIONES MATRIZ ST -----------------------------
+typedef struct
+{
+	char nombre[60];
+	float nota;
+} tficha2;
+
+void static matrizSt()
+{
+	static tficha2 alumno[NA];	// matriz estructura
+	int n = 0, i = 0;
+	char *fin = NULL;  // para almacenar gets
+	int aprobados = 0, suspendidos = 0;
+
+	//entrada de datos
+	printf("Introducir Datos");
+	printf("Para finalizar introduzca ~");
+
+	printf("Nombre");
+	fin = gets(alumno[n].nombre);
+	printf("%s", alumno[n].nombre);
+}
--- Curso_cpp/curso/main.cpp
+++ Curso_cpp/curso/main.cpp
@@ -4,23 +4,100 @@
 #include <stdlib.h>
 using namespace std;
 
+int SALIR = 0;
+
 //printf("\n-texto-\n");
+
+void static menuCap6()
+{	
+	system("cls");
+	printf("\n\t**MENU DE Capitulo 6 C++**\n\n");
+	printf("\t1 - Ap1 - Notas Medias\n\n");
+	printf("\t2 - Ap2 - Matriz Unidimensional\n\n");
+	printf("\t3 - Ap3 - Contador de apariciones de letras\n\n");
+	printf("\t4 - Ap4 - Entrada con get\n\n");
+	printf("\t5 - Ap5 - Matriz Bidimensional\n\n");
+	printf("\t6 - Ap6 - Copia de matrices\n\n");
+	printf("\t7 - Ap7 - Estructuras\n\n");
+	printf("\t8 - Ap8 - Matriz St\n\n");
+	printf("\tv - Volver\n\n");
+	printf("Elija una opcion :  ");
+
+	// elegir opecion del menu	
+	int cara=0;
+	scanf("%d", &cara);
+
+	switch (cara)
+	{
+	case 1:
+		system("cls");
+		//Pide notas x teclado,y calcula la media
+		nota_media();
+		break;
+	case 2:
+		system("cls");
+		matrizUni(); /*matriz unidimensional*/
+		break;
+	case 3:
+		system("cls");
+		letras();  /* contador de apariciones de una letra en texto*/
+		break;
+	case 4:
+		system("cls");
+		getCpp();	  /* entrada de teclado con get*/
+		break;
+	case 5:
+		system("cls");
+		matrizBi();	  /* matriz bidimentnsional*/
+		break;
+	case 6:
+		system("cls");
+		copiarMatrices();  /* Copiar matrices */
+		break;
+	case 7:
+		system("cls");
+		estructuras();		/* estructuras */
+		break;
+	case 8:
+		system("cls");
+		matrizSt();		/* Matriz St */
+		break;
+	case 'v':
+		break;
+	}
+
+	//cap 6 - Estructura de datos ----------------------------------------
+}
 
 int main()
 {
-	printf("**Inicio de la Aplicacion **\n");
-	// archivos y funciones realizadas a lo largo del curso
-	//active las que desee utilizar
+	char car;
 	
-	//Pide notas x teclado,y calcula la media
-	//nota_media();
+		system("cls");
+		printf("\n\t**MENU DE CURSO C++**\n\n");
+		printf("\t1 - Capitulo 6 - Estructuras de datos\n\n");
+		printf("\tq - Salir\n\n");
+		printf("\tEscoja una opcion :  ");
 
-	//cap 6 - Estructura de datos
-	//matrizUni(); // matriz unidimensional
-	//letras();    // contador de apariciones de una letra en texto
-	//getCpp();	  // entrada de teclado con get
-	matrizBi();	  // matriz bidimentnsional
+		// elegir opecion del menu	
+		fflush(stdout);
+		fflush(stdin);
+		car = getchar();
+		fflush(stdout);
+		fflush(stdin);
+		// Swuitch
+		switch (car)
+		{
+		case '1':
+			// Estructura de datos
+			menuCap6();
+			system("pause");
+			break;
+		case 'q':			
+			break;	
+	}	
+	//--------------------------------------------------------------------
 	
-	system("pause");
+	//system("pause");
 	return 0;
 }
